64c7dff4baec7621fa4a81b432f08e6283a32a0f
yoori <yuri.kuznecov@gmail.com>
2024-10-08 17:36:12 +0000

    x

--- deps/make.sh
+++ deps/make.sh
@@ -10,7 +10,7 @@ pushd rocksdb
 export LUA_PATH=
 export LUA_INCLUDE=
 # make clean
-make static_lib -j$(nproc)
+make static_lib DISABLE_WARNING_AS_ERROR=1 -j$(nproc)
 #make shared_lib
 # make check
 popd
